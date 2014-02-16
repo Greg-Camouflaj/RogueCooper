701babe723955f8f8f2376275217393cdaf28100
Greg-Camouflaj <greg@camouflaj.com>
2014-02-15 18:37:13 -0800

    Contagion Move, Vector2Int coordinate helper, etc.

--- RogueCooperTest/Assets/Scripts/GameBoard.cs
+++ RogueCooperTest/Assets/Scripts/GameBoard.cs
@@ -31,9 +31,46 @@ public class GameBoard : MonoBehaviour
         }
     }
 
+    public GameCube GetGameCube(int x, int y)
+    {
+        return _gameCubes[y * GAME_BOARD_DIMENSION + x];
+    }
+
+    public GameCube GetGameCube(Vector2Int position)
+    {
+        return GetGameCube(position.x, position.y);
+    }
+
     public void SetOwner(int x, int y, GameLogic.Owner owner)
     {
         _gameCubes[y * GAME_BOARD_DIMENSION + x].SetOwner(owner);
+    }
+
+    public void SetOwner(Vector2Int position, GameLogic.Owner owner)
+    {
+        SetOwner(position.x, position.y, owner);
+    }
+
+    public bool IsInBounds(int x, int y)
+    {
+        return
+            (   0 <= x && x < GAME_BOARD_DIMENSION &&
+                0 <= y && y < GAME_BOARD_DIMENSION);
+    }
+
+    public bool IsInBounds(Vector2Int position)
+    {
+        return IsInBounds(position.x, position.y);
+    }
+
+    public GameLogic.Owner GetOwner(int x, int y)
+    {
+        return GetGameCube(x, y).Owner;
+    }
+
+    public GameLogic.Owner GetOwner(Vector2Int position)
+    {
+        return GetOwner(position.x, position.y);
     }
 
     public void GetOwnerCounts(out int contagionOut, out int playerCount)
@@ -52,5 +89,31 @@ public class GameBoard : MonoBehaviour
                 ++contagionOut;
             }
         }
+    }
+
+    public void GetContagionCubes(out List<Vector2Int> contagionCubes)
+    {
+        contagionCubes = new List<Vector2Int>();
+
+        for (int y = 0; y < GAME_BOARD_DIMENSION; y++)
+        {
+            for (int x = 0; x < GAME_BOARD_DIMENSION; x++)
+            {
+                if (_gameCubes[y * GAME_BOARD_DIMENSION + x].Owner == GameLogic.Owner.Contagion)
+                {
+                    contagionCubes.Add(new Vector2Int(x, y));
+                }
+            }
+        }
+    }
+
+    public bool IsValidContagionMove(int x, int y)
+    {
+        return (IsInBounds(x, y) && GetGameCube(x, y).Owner != GameLogic.Owner.Player);
+    }
+
+    public bool IsValidContagionMove(Vector2Int position)
+    {
+        return IsValidContagionMove(position.x, position.y);
     }
 }
---
+++ RogueCooperTest/Assets/Scripts/Utils/Vector2Int.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vector2Int
+{
+    private int _x;
+    public int x
+    {
+        get { return _x; }
+        set { _x = value; }
+    }
+
+    private int _y;
+    public int y
+    {
+        get { return _y; }
+        set { _y = value; }
+    }
+
+    public Vector2Int(int X, int Y)
+    {
+        _x = X;
+        _y = Y;
+    }
+
+    public Vector2Int GetLeft()
+    {
+        return new Vector2Int(_x - 1, _y);
+    }
+
+    public Vector2Int GetRight()
+    {
+        return new Vector2Int(_x + 1, _y);
+    }
+
+    public Vector2Int GetUp()
+    {
+        return new Vector2Int(_x, _y - 1);
+    }
+
+    public Vector2Int GetDown()
+    {
+        return new Vector2Int(_x, _y + 1);
+    }
+}
