bd56e5388b5e61a52732e21aab2bcfcce5ef62d8
Greg-Camouflaj <greg@camouflaj.com>
2014-02-15 17:01:00 -0800

    Moved generator into Game Board

--- RogueCooperTest/Assets/Scripts/GameBoard.cs
+++ RogueCooperTest/Assets/Scripts/GameBoard.cs
@@ -5,27 +5,25 @@ public class GameBoard : MonoBehaviour
 {
     public const int GAME_BOARD_DIMENSION = 12;
 
-
-    private GameCube[] _gameCubes = null;
-
     public void GenerateGameBoard()
     {
-		CreateGameBoardCubes();
-        PositionGameBoardCubes();
-    }
+        int halfDimension = GAME_BOARD_DIMENSION / 2;
+        int startingIndex = -1 * halfDimension;
+        int endingIndex = halfDimension;
 
-    private void CreateGameBoardCubes()
-    {
-        _gameCubes = new GameCube[GAME_BOARD_DIMENSION * GAME_BOARD_DIMENSION];
-    }
-
-    private void PositionGameBoardCubes()
-    {
-        for (int y = 0; y < GAME_BOARD_DIMENSION; ++y)
+        //This outer loop tracks our vertical position
+        for (int j = startingIndex; j < endingIndex; j++)
         {
-            for (int x = 0; x < GAME_BOARD_DIMENSION; ++x)
+            //Create all our cubes in a horizontal line
+            for (int i = startingIndex; i < endingIndex; i++)
             {
-                _gameCubes[y * GAME_BOARD_DIMENSION + x].transform.position = Vector3.zero;
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //NOTE: The following function creates a NEW material on every call
+                //TODO: Specifically create these materials before hand and reference them here
+                //cube.renderer.sharedMaterial = ...
+                cube.renderer.material.shader = Shader.Find("Self-Illumin/Diffuse");
+                cube.renderer.material.SetColor("_Color", Color.red);
+                cube.transform.position = new Vector3(0.5F + i + (i * 0.1F), 0.5F + j + (j * 0.1F), 0);
             }
         }
     }
